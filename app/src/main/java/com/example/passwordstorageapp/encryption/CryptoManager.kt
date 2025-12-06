0a76331fb10fc8bfeb33001dbac5561f4c5f83a4
ByteSizedMe <malhar.srivatsav@gmail.com>
2025-12-06 14:56:11 +0530

    Encryption/ Decryption setup

--- app/src/main/java/com/example/passwordstorageapp/encryption/CryptoManager.kt
+++ app/src/main/java/com/example/passwordstorageapp/encryption/CryptoManager.kt
@@ -7,18 +7,38 @@ import javax.crypto.spec.GCMParameterSpec
 import java.security.SecureRandom
 import java.nio.charset.StandardCharsets
 
-// in progress
-
 class CryptoManager(
     private val key : ByteArray
 ) {
 
-    fun encrypt(plainText : String){
+    init{
+        require(key.size==32){
+            "Expected 32-byte key for AES-256, got ${key.size}"
+        }
+    }
 
+    fun encrypt(plainText : String) : EncryptedData{
+        val secretKey = SecretKeySpec(key, "AES")
+        val iv = ByteArray(12).also{
+            SecureRandom().nextBytes(it)
+        }
+        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, GCMParameterSpec(128,iv))
+        val cipherText = cipher.doFinal(plainText.toByteArray(StandardCharsets.UTF_8))
+        return EncryptedData(iv, cipherText)
     }
 
     fun decrypt(encryptedData: EncryptedData) : String{
-        return "test"
+        require(encryptedData.iv.size==12){
+            "Invalid IV size: ${encryptedData.iv.size}"
+        }
+        val secretKey = SecretKeySpec(key, "AES")
+        val cipher = Cipher.getInstance("AES/GCM/NoPadding")
+        val gcmSpec = GCMParameterSpec(128, encryptedData.iv)
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, gcmSpec)
+        val plainBytes = cipher.doFinal(encryptedData.cipherText)
+        val plainText = String(plainBytes, StandardCharsets.UTF_8)
+        return plainText
     }
 }
 
